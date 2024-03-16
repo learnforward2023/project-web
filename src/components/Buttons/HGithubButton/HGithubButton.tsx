f05b25d24d6f22bf8780e8ab4ce3b08824895156
Learn Forward 2023 <learnforward2023@gmail.com>
2024-03-16 23:24:18 +0900

    Update border color for sns button

--- src/components/Buttons/HGithubButton/HGithubButton.tsx
+++ src/components/Buttons/HGithubButton/HGithubButton.tsx
@@ -14,7 +14,7 @@ const HGithubButton: React.FC<IHGithubButtonProps> = ({
 }) => {
   return (
     <button
-      className={`inline-flex gap-2 items-center border border-transparent text-sm rounded-md tracking-widest disabled:opacity-45 transition${className} p-2 text-black hover:bg-gray-100 focus:border-gray-300 focus:ring focus:ring-opacity-50 focus:ring-gray-200 flex-1 justify-center items-center gap-2 border-gray-200`}
+      className={`inline-flex gap-2 items-center border border-transparent text-sm rounded-md tracking-widest disabled:opacity-45 transition${className} p-2 text-black hover:bg-gray-100 focus:border-gray-300 focus:ring focus:ring-opacity-50 focus:ring-gray-200 flex-1 justify-center items-center gap-2 border-gray-300`}
       disabled={disabled}
       onClick={onClick}
     >
